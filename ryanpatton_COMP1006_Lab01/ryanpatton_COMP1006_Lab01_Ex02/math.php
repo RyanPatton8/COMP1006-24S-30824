6fb62b93939af1e41ae7df376203cba8bbf25f3e
RyanPatton8 <116020479+RyanPatton8@users.noreply.github.com>
2024-05-28 11:47:32 -0400

    Create math.php

---
+++ ryanpatton_COMP1006_Lab01/ryanpatton_COMP1006_Lab01_Ex02/math.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 02</title>
+</head>
+<body>
+    <?php 
+        $grades = array(60, 70, 40, 90, 50);
+
+        highest($grades);
+    
+        function highest($arr){
+            // find highest number 
+            echo"{$arr[0]}";
+        }
+
+        function lowest($arr){
+
+        }
+
+        function average($arr){
+
+        }
+    ?>
+</body>
+</html>
