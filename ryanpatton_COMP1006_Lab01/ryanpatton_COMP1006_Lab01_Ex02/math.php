37b72a01bd9db16c8f6f7c0bf857b580b175618d
RyanPatton8 <116020479+RyanPatton8@users.noreply.github.com>
2024-05-28 12:10:23 -0400

    Update math.php

--- ryanpatton_COMP1006_Lab01/ryanpatton_COMP1006_Lab01_Ex02/math.php
+++ ryanpatton_COMP1006_Lab01/ryanpatton_COMP1006_Lab01_Ex02/math.php
@@ -9,20 +9,49 @@
     <?php 
         $grades = array(60, 70, 40, 90, 50);
 
-        highest($grades);
-    
         function highest($arr){
             // find highest number 
-            echo"{$arr[0]}";
+            $highest = 0;
+            // I know I could just print the maximum number using the max method on the whole array
+            // but I felt like you'd prefer this for the purpose of the assignment
+            foreach ($arr as $grade) {
+                //max function returns the largest number from an array
+                $highest = max($highest, $grade);
+            }
+
+            echo "the highest grade is {$highest} <br>";
         }
 
         function lowest($arr){
+            $lowest = 101;
 
+            foreach ($arr as $grade) {
+                //min function returns the smallest number in an array
+                $lowest = min($lowest, $grade);
+            }
+            
+            echo "the lowest grade is {$lowest} <br>";
         }
 
+        //adds each number in an array together and divides by the number of numbers
         function average($arr){
+            $count = 0;
+            $total = 0;
 
+            foreach ($arr as $grade) {
+                $total = $total + $grade;
+                $count++;
+            }
+
+            $total = $total / $count;
+
+            echo "the class average is {$total} <br>";
         }
+
+
+        highest($grades);
+        lowest($grades);
+        average($grades);
     ?>
 </body>
 </html>
